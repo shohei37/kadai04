19733c68ddcd89cc22122850170df33c82bedb72
shohei37 <102854135+shohei37@users.noreply.github.com>
2022-04-29 06:28:29 +0900

    Add files via upload

---
+++ 課題04/chat.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAWxrMeFmUq_W3oHWBtl8Yq35yWk2FQ0P8",
+    authDomain: "othello-bd15b.firebaseapp.com",
+    databaseURL: "https://othello-bd15b-default-rtdb.firebaseio.com",
+    projectId: "othello-bd15b",
+    storageBucket: "othello-bd15b.appspot.com",
+    messagingSenderId: "293403770318",
+    appId: "1:293403770318:web:89bedf757199d46f213f9f"
+  };
+
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
